b0efb91df12fda963c2169bdf925454db6c5f026
maciejgawlowski <Mx3va111>
2019-09-08 18:48:48 +0200

    refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,12 @@ import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { MainSearchComponent } from './main-search/main-search.component';
+import { MainPageComponent } from './main-page/main-page.component';
 import {MatButtonModule, MatFormFieldModule, MatInputModule, MatSelectModule} from '@angular/material';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {CarsListComponent} from "./cars-list/cars-list.component";
 import {RouterModule} from "@angular/router";
+import {MainSearchComponent} from "./main-page/main-search/main-search.component";
 
 @NgModule({
   imports: [
@@ -24,13 +25,14 @@ import {RouterModule} from "@angular/router";
     MatSelectModule,
     MatButtonModule,
     RouterModule.forRoot([
-      { path: '', component: MainSearchComponent },
+      { path: '', component: MainPageComponent },
       {path: 'cars', component: CarsListComponent}
     ])
   ],
    declarations: [
       AppComponent,
       NavBarComponent,
+      MainPageComponent,
       MainSearchComponent,
       CarsListComponent
    ],
